850c813ea5f41ccd7a5ea93291abef40d83dee64
beafshar <baharafshar2079@gmail.com>
2022-02-25 23:15:19 +0330

    tests for voting comments added.

--- src/test/java/CommentTest.java
+++ src/test/java/CommentTest.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class CommentTest {
+
+import org.json.JSONException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.json.JSONObject;
+
+public class CommentTest {
+    UserHandler users;
+    User user;
+    Comment comment;
+
+    @Before
+    public void setup()
+    {
+        user = new User("Mahdi@gmail.com","1234", "mahdi", "mahdi mahdavi","2000-02-01");
+        users = new UserHandler();
+//        users.addUser(user);
+        comment = new Comment();
+    }
+
+    @After
+    public void teardown()
+    {
+        user = null;
+        comment = null;
+    }
+
+    @Test
+    public void userSuccessfullyVotesToAComment() throws JSONException {
+        int likes = comment.getLikes();
+        int dislikes = comment.getDislikes();
+        JSONObject response = comment.addVote(user.getEmail(), 1);
+        //assert response
+        assertEquals(likes + 1, comment.getLikes());
+        assertEquals(dislikes, comment.getDislikes());
+    }
+
+    @Test
+    public void userVotesACommentTwice() throws JSONException {
+        int likes = comment.getLikes();
+        int dislikes = comment.getDislikes();
+        JSONObject response = comment.addVote(user.getEmail(), 1);
+        response = comment.addVote(user.getEmail(), -1);
+        //assert response
+        assertEquals(likes, comment.getLikes());
+        assertEquals(dislikes + 1, comment.getDislikes());
+    }
+
+    @Test
+    public void userNotFound() throws JSONException {
+        int likes = comment.getLikes();
+        int dislikes = comment.getDislikes();
+        JSONObject response = comment.addVote("beafshar@gmail.com", 1);
+        //assert response
+        assertEquals(likes, comment.getLikes());
+        assertEquals(dislikes, comment.getDislikes());
+    }
+
+    @Test
+    public void givenCommentNotFound()
+    {
+
+    }
+
+    @Test
+    public void InvalidVoteScore() throws JSONException {
+        int likes = comment.getLikes();
+        int dislikes = comment.getDislikes();
+        JSONObject response = comment.addVote(user.getEmail(), 7);
+        //assert response
+        assertEquals(likes, comment.getLikes());
+        assertEquals(dislikes, comment.getDislikes());
+    }
 }
