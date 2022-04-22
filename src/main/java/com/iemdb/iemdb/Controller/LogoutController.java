7468a2f08c25ccbc3c47d04143757bd00c0b3fd7
fatemedbghi <fatemehdbghiii@gmail.com>
2022-04-22 13:57:30 +0430

    fix logout controller

--- src/main/java/com/iemdb/iemdb/Controller/LogoutController.java
+++ src/main/java/com/iemdb/iemdb/Controller/LogoutController.java
@@ -1,22 +1,19 @@
-//package com.myservlet.Controller;
-//
-//import com.myservlet.Model.Error.MovieNotFound;
-//import com.myservlet.Model.Error.UserNotFound;
-//import com.myservlet.Model.IEMDBController;
-//import javax.servlet.*;
-//import javax.servlet.http.*;
-//import javax.servlet.annotation.*;
-//import java.io.IOException;
-//
-//@WebServlet("/logout")
-//public class LogoutController extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        try {
-//            IEMDBController.getInstance().setActive_user("");
-//            response.sendRedirect("/login");
-//        } catch (InterruptedException | MovieNotFound | UserNotFound e) {
-//            request.getRequestDispatcher("404.html").forward(request, response);
-//        }
-//    }
-//}
+package com.iemdb.iemdb.Controller;
+
+import com.iemdb.iemdb.Model.Error.MovieNotFound;
+import com.iemdb.iemdb.Model.Error.UserNotFound;
+import com.iemdb.iemdb.Model.IEMDBController;
+import com.iemdb.iemdb.Model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/logout")
+public class LogoutController {
+
+    @PostMapping("")
+    public User logout() throws IOException, MovieNotFound, InterruptedException, UserNotFound {
+        return IEMDBController.getInstance().setActive_user("", "");
+    }
+}
